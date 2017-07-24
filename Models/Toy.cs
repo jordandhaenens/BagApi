d4389d1905353bf004b6e990d103e43e1f4ffc94
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-07-24 10:54:58 -0500

    first commit

---
+++ Models/Toy.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BagAPI.Models
+{
+    public class Toy
+    {
+        [Key]
+        public int ToyId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int ChildId { get; set; }
+        public Child Child { get; set; }
+    }
+}
